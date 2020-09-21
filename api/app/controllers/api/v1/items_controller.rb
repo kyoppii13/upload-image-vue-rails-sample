5e677ec8cc405f21beacb7509cd094dd9808d190
Kyou13 <kyou1013@gmail.com>
2020-09-21 19:34:20 +0900

    show action

--- api/app/controllers/api/v1/items_controller.rb
+++ api/app/controllers/api/v1/items_controller.rb
@@ -1,9 +1,25 @@
 module Api
   module V1
     class ItemsController < ApplicationController
+      before_action :set_item, only: [:show]
+
       def index
         @items = Item.all
         render json: @items
+      end
+
+      def show
+        render json: @item
+      end
+
+      private
+
+      def set_item
+        @item = Item.find(params[:id])
+      end
+
+      def item_params
+        params.require(:item).permit(:title, :image_url)
       end
     end
   end

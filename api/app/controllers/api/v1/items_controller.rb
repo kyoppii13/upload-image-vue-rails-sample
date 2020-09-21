9b00685774eb763f00e2370816c1dd52b85bbfde
Kyou13 <kyou1013@gmail.com>
2020-09-21 20:00:00 +0900

    destroy, update action

--- api/app/controllers/api/v1/items_controller.rb
+++ api/app/controllers/api/v1/items_controller.rb
@@ -1,7 +1,7 @@
 module Api
   module V1
     class ItemsController < ApplicationController
-      before_action :set_item, only: [:show]
+      before_action :set_item, only: [:show, :destroy, :update]
 
       def index
         @items = Item.all
@@ -22,9 +22,19 @@ module Api
       end
 
       def destroy
+        if @item.destroy
+          render json: @item
+        else
+          render json: @item.errors
+        end
       end
 
       def update
+        if @item.update(item_params)
+          render json: @item
+        else
+          render json: @item.errors
+        end
       end
 
       private
